557bfa1a3a8b61c2797932d649dc21542eeda75e
SAURAV <63091714+saurav203@users.noreply.github.com>
2020-04-30 17:29:06 -1200

    Add files via upload

---
+++ HCFGCD.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,i,gcd;
+printf("enter the number");
+scanf("%d%d",&n1,&n2);
+for(i=1;i<=n;&&<=n2;i++)
+{
+if(n%i=0;&&n2%i==0)
+gcd=i;
+}
+printf("gcd of %d %d",n1,n2,gcd);
+getch();
+}
