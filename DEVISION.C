0a9d2897fa1f26d1f00f59bdafc0e93c208c2a60
SAURAV <63091714+saurav203@users.noreply.github.com>
2020-04-30 17:21:41 -1200

    Add files via upload

---
+++ DEVISION.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+double num1,num2;
+int result;
+printf("enter the num1");
+scanf("%1f",&num1);
+printf("enter the num2");
+scanf("%1f",&num2);
+result=num1/num2;
+printf("result is integer division is: %d",result);
+}
