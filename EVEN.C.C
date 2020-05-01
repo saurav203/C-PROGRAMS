83e968760dc17867b2bfaed56bb39045b0a93fe9
SAURAV <63091714+saurav203@users.noreply.github.com>
2020-04-30 17:27:21 -1200

    Add files via upload

---
+++ EVEN.C.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter a number");
+scamf("%d",&n);
+if(n%2==0)
+printf("it is an even number");
+else
+printf("it is not an even number");
+}
+getch();
