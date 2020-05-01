d18b7813b8474ecb5ab49e567583635bb1270a70
SAURAV <63091714+saurav203@users.noreply.github.com>
2020-04-30 16:55:25 -1200

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number1,number2,result;
+printf("enter the first number");
+scanf("%d",&number2);
+printf("enter the second number");
+scanf("%d",&number2);
+result=number1+number2;
+printf("result of addition is:%d",result);
+}
