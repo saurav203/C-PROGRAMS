52afc80ff4c0cd7d5284a4320bb5be0ab4c26942
SAURAV <63091714+saurav203@users.noreply.github.com>
2020-04-30 17:45:25 -1200

    Add files via upload

---
+++ WEARSUITE.C.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char suite=3;
+switch(suite)
+{
+case 1:
+printf("diaomand\n");
+case 2:
+printf("spade\n");
+default:
+printf("heart\n");
+}
+printf("i through one wears a suite\n");
+return 0;
+}
